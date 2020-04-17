16a86fefed31cb498dbb4e5f21f8acc7b08946cd
chance <bluenote212@naver.com>
2020-04-17 18:25:43 +0900

    0417

--- history.py
+++ history.py
@@ -1,45 +1,3 @@
-user = pd.read_sql("SELECT * FROM id_pw", con)
-user_info = user.values.tolist()
-con.close()
-userData = {'os_username': user_info[0][0], 'os_password': user_info[0][1]}
-
-#Team code
-team_code = [
-        ['SOC Advanced Team', '4', 'DEPT173'],
-        ['SOC IP Design Team', '95', 'DEPT188'],
-        ['SOC Design Team', '5', 'TCW01600'],
-        ['SOC Verification Team', '6', 'DEPT81'],
-        ['SOC Implementation Team', '8', 'TCW01420'],
-        ['HW Platform Team', '87', 'TCW03300'],
-        ['HW Verification Team', '88', 'DEPT180'],
-        ['System BSP Team', '10', 'TCW02900'],
-        ['Application BSP Team', '11', 'TCW02203'],
-        ['Security solution Team', '9', 'TCW02700'],
-        ['Media Android Team', '89', 'DEPT182'],
-        ['Media Linux Team', '90', 'TCW01230'],
-        ['Media HAL Team', '91', 'DEPT183'],
-        ['Automotive MCU Team', '22', 'TCW03100'],
-        ['Wireless Team', '3', 'TCW02070'],
-        ['Bluetooth Team', '19', 'DEPT75'],
-        ['SW Architecture Team', '14', 'DEPT175'],
-        ['Project Management Team', '92', 'DEPT184'],
-        ['STB Platform Team', '93', 'TCW03110'],
-        ['Automotive Platform Team', '15', 'TCW02500'],
-        ['Driver Assistance Platform Team', '18', 'TCW02400'],
-        ['RND Innovation Team', '2', 'TCW04300'],
-        ['Technical Writing Team', '94','DEPT186']
-        ]
-
-
-data = pd.DataFrame(team_code, columns = ['team','team_code', 'group_code'])
-
-con = sqlite3.connect('C:/Users/B180093/database/tcs.db')
-data.to_sql('team_code', con, if_exists='replace', index = False)
-con.close()
-runfile('C:/Users/B180093/.spyder-py3/allteam_resource2.py', wdir='C:/Users/B180093/.spyder-py3')
-runfile('C:/Users/B180093/.spyder-py3/allteam_resource_wikicreate2.py', wdir='C:/Users/B180093/.spyder-py3')
-
-## ---(Fri Apr 10 13:43:38 2020)---
 runfile('C:/Users/B180093/.spyder-py3/allteam_resource2.py', wdir='C:/Users/B180093/.spyder-py3')
 runfile('C:/Users/B180093/.spyder-py3/allteam_resource_wikicreate2.py', wdir='C:/Users/B180093/.spyder-py3')
 runfile('C:/Users/B180093/.spyder-py3/user_dataDB_create.py', wdir='C:/Users/B180093/.spyder-py3')
@@ -140,4 +98,26 @@ runfile('C:/Users/B180093/.spyder-py3/test.py', wdir='C:/Users/B180093/.spyder-p
 print(project_data[94])
 print(project_data[93])
 runfile('C:/Users/B180093/.spyder-py3/page_body_storage_info.py', wdir='C:/Users/B180093/.spyder-py3')
+runfile('C:/Users/B180093/.spyder-py3/test.py', wdir='C:/Users/B180093/.spyder-py3')
+
+## ---(Fri Apr 17 11:10:48 2020)---
+runfile('C:/Users/B180093/.spyder-py3/test.py', wdir='C:/Users/B180093/.spyder-py3')
+print(project_data)
+runfile('C:/Users/B180093/.spyder-py3/test.py', wdir='C:/Users/B180093/.spyder-py3')
+print(project_data)
+runfile('C:/Users/B180093/.spyder-py3/test.py', wdir='C:/Users/B180093/.spyder-py3')
+print(data[0]['versions'])
+runfile('C:/Users/B180093/.spyder-py3/test.py', wdir='C:/Users/B180093/.spyder-py3')
+print(project)
+print(project_key)
+runfile('C:/Users/B180093/.spyder-py3/test.py', wdir='C:/Users/B180093/.spyder-py3')
+print(type(url))
+print(type(url.text))
+data.append(url.text)
+runfile('C:/Users/B180093/.spyder-py3/test.py', wdir='C:/Users/B180093/.spyder-py3')
+print(data)
+runfile('C:/Users/B180093/.spyder-py3/test.py', wdir='C:/Users/B180093/.spyder-py3')
+print(project_key[0])
+runfile('C:/Users/B180093/.spyder-py3/test.py', wdir='C:/Users/B180093/.spyder-py3')
+print(data)
 runfile('C:/Users/B180093/.spyder-py3/test.py', wdir='C:/Users/B180093/.spyder-py3')
--- test.py
+++ test.py
@@ -16,9 +16,24 @@ confluence = Confluence(
     username = user_info[0][0],
     password = user_info[0][1])
 
-#profield data 
+con = sqlite3.connect('C:/Users/B180093/database/tcs.db')
+project = pd.read_sql("SELECT * FROM project_info WHERE category = '1.SOC 개발' or category = '2.SOC 검증' or category = '3.SDK 개발' or category = '4.요소/기반 기술' or category = '5.사업자/선행/국책' or category = '6.HW개발' ", con)
+project_key = project.values.tolist()
+con.close()
+
+data = []
+
+for h in range(0, len(project_key)):
+    url = requests.get('https://tcs.telechips.com:8443/rest/profields/api/2.0/values/projects/' + str(project_key[h][0]) + '?calculated=true', userData)
+    data.append(json.loads(url.text))
+
+print(data)
+
+'''
+#profield data에서 project data만 추출
 url = requests.get('https://tcs.telechips.com:8443/rest/profields/api/2.0/layouts/projects', userData)
 data = json.loads(url.text)
+
 
 project_data = []
 for i in range(0, len(data)):
@@ -31,12 +46,6 @@ for i in range(0, len(data)):
                 a.append(data[i]['fields'][j]['value']['value'])
         
         if data[i]['fields'][j]['field']['id'] == -1:
-            if data[i]['fields'][j]['value'] is None:
-                a.append('None')
-            else:
-                a.append(data[i]['fields'][j]['value']['value'])
-        
-        if data[i]['fields'][j]['field']['id'] == -4:
             if data[i]['fields'][j]['value'] is None:
                 a.append('None')
             else:
@@ -60,17 +69,17 @@ for i in range(0, len(data)):
             else:
                 a.append(data[i]['fields'][j]['value']['value']['value'])
         
-        if data[i]['fields'][j]['field']['id'] == 34:
-            if data[i]['fields'][j]['value'] is None:
-                a.append('None')
-            else:
-                a.append(data[i]['fields'][j]['value']['value'])
-        
         if data[i]['fields'][j]['field']['id'] == 35:
             if data[i]['fields'][j]['value'] is None:
                 a.append('None')
             else:
                 a.append(data[i]['fields'][j]['value']['value'])
+
+        if data[i]['fields'][j]['field']['id'] == 34:
+            if data[i]['fields'][j]['value'] is None:
+                a.insert(5, 'None')
+            else:
+                a.insert(5, data[i]['fields'][j]['value']['value'])
         
         if data[i]['fields'][j]['field']['id'] == 33:
             if data[i]['fields'][j]['value'] is None:
@@ -86,18 +95,23 @@ for i in range(0, len(data)):
 
     project_data.append(a)
 
-#Wiki 페이지에 Data 생성
+#Wiki 페이지에 Project Data page 생성
 wiki_data_top = '<p class="auto-cursor-target"><br /></p><ac:structured-macro ac:name="table-excerpt" ac:schema-version="1" ac:macro-id="70b8954e-f54e-46e7-8b34-a176d7c406ee">\
-<ac:parameter ac:name="name">project_data</ac:parameter><ac:rich-text-body><p class="auto-cursor-target"><br /></p><table><colgroup><col /><col /><col /><col /><col />\
-<col /><col /><col /><col /><col /></colgroup><tbody><tr><th>Name</th><th>Key</th><th>Url</th><th>PL</th><th>Category</th><th>Chip</th><th>start_date</th>\
+<ac:parameter ac:name="name">project_data</ac:parameter><ac:rich-text-body><p class="auto-cursor-target"><br /></p><table><colgroup><col /><col /><col /><col />\
+<col /><col /><col /><col /><col /></colgroup><tbody><tr><th>Name</th><th>Key</th><th>PL</th><th>Category</th><th>Chip</th><th>start_date</th>\
 <th>due_date</th><th>Status</th><th>Wiki</th></tr>'
 wiki_data_middle = ''
 wiki_data_bottom = '</tbody></table><p class="auto-cursor-target"><br /></p></ac:rich-text-body></ac:structured-macro><p><br /></p>'
 
 for i in range(0, len(project_data)):
     data_row = '<tr>'
-    for j in range(0, len(project_data[i])):
-        data_row += '<td>' + project_data[i][j] + '</td>'
+    for j in range(0, 9):
+        if j == 0:
+            data_row += '<td><a href="https://tcs.telechips.com:8443/projects/' + project_data[i][1] + '/summary/statistics">' + project_data[i][j] + '</a></td>'
+        elif j== 8 and project_data[i][8] != 'None':
+            data_row += '<td><a href="' + project_data[i][8] + '">Wiki</a></td>'
+        else:
+            data_row += '<td>' + project_data[i][j] + '</td>'
     data_row += '</tr>'
     wiki_data_middle += data_row
 
@@ -105,10 +119,12 @@ wiki = wiki_data_top + wiki_data_middle + wiki_data_bottom
 wiki = wiki.replace("&","<p>&amp;</p>")
 
 confluence.update_page(
-        parent_id = 99890426,
+        parent_id = 95455710,
         page_id = 120297594,
         title = 'project_data',
         body = wiki,
         type='page',
         representation='storage'
     )
+'''
+
